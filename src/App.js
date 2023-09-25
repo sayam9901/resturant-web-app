075a54090a7f5ecebfcafc2864382817567575fb
sayam9901 <sayamkhan9560621912@gmail.com>
2023-09-25 19:47:29 +0530

    adding summary to the website

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 
 import Header from "./components/Layout/Header";
+import Meals from "./components/Meals/Meals"
 function App() {
   return (
-    <div >
+    < >
       <Header/>
-    </div>
+      <main>
+        <Meals/>
+      </main>
+    </>
   );
 }
 
